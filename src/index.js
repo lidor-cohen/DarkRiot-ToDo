5d348b448011722a2d3eaf15d48e42db08940a45
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-24 23:19:50 +0300

    initialize new backend branch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import Task from "./js/task.js";
-import TaskController from "./js/task-controller.js";
-import "./js/menu-animations.js";
-import { createSection } from "./js/content-controller.js";
+import "./js/css-imports";
 
-import "./styles/main.scss";
-import "./styles/menu.scss";
-import "./styles/buttons.scss";
-import "./styles/content.scss";
-import "./styles/task.scss";
-import DomHandler from "./js/dom-handler.js";
-
-function init() {
-  TaskController.addRandomTasks(3);
-  createSection("Army");
-
-  DomHandler.updateAll();
-}
+function init() {}
 
 init();
---
+++ src/js/css-imports.js
@@ -0,0 +1,6 @@
+// CSS Imports
+import "../styles/main.scss";
+import "../styles/menu.scss";
+import "../styles/buttons.scss";
+import "../styles/content.scss";
+import "../styles/task.scss";

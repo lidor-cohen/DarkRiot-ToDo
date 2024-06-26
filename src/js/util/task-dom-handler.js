6f812f193e8939237458c6203120ea2c5717bd85
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-26 18:10:06 +0300

    added priority slider to addtask

--- src/js/util/task-dom-handler.js
+++ src/js/util/task-dom-handler.js
@@ -1,11 +1,33 @@
+import { taskController } from "../classes/task-controller";
+
 function TaskWindowController() {
   this.addTaskButton = document.getElementById("add-task-wrapper");
   this.taskDialog = document.getElementById("task-dialog-wrapper");
+  this.categorySelect = document.getElementById("dialog-input-category");
+  this.priorityLabel = document.getElementById("priority-label");
+  this.prioritySlider = document.getElementById("dialog-input-priority");
 }
 
 TaskWindowController.prototype = {
-  openNewTask: function () {},
   bindEvents: function () {
+    // Priority Range
+    this.prioritySlider.addEventListener(
+      "input",
+      (e) =>
+        (this.priorityLabel.textContent = "Priority (" + e.target.value + ")")
+    );
+
+    // Category Selector
+    this.categorySelect.innerHTML = "";
+    taskController.getUniqueCategories().forEach((category) => {
+      const optionItem = `
+        <option value="${category}">${category}</option>
+        `;
+
+      this.categorySelect.innerHTML += optionItem;
+    });
+
+    // Add Task Button
     this.addTaskButton.addEventListener(
       "click",
       () => this.taskDialog.showModal(),
